d6873f98d13f5777c45ffbece7f5731c2d2a5e58
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-30 21:41:10 -0300

    add components/Header styles

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  nav {
+    display: flex;
+    justify-content: center;
+    background-color: var(--white-200);
+    box-shadow: 0px 1px 5px 0 var(--gray-300);
+
+    ul {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 2rem 0.8rem;
+      gap: 2rem;
+
+      li {
+        list-style-type: none;
+
+        a {
+          text-decoration: none;
+          text-transform: uppercase;
+          font-weight: bold;
+          color: var(--gray-300);
+          font-size: 1.3rem;
+          padding-bottom: 0.1rem;
+          transition: all ease 0.2s;
+          opacity: 0.7;
+
+          &:hover {
+            border-bottom: 1.5px solid var(--gray-300);
+            opacity: 1;
+          }
+        }
+      }
+    }
+  }
+`;
