debef318ca87eb82eb1456361dd9eb834f61a78a
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-31 22:38:58 -0300

    feat pages/Login styles

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,10 +1,6 @@
 import styled from "styled-components";
 
-interface IInputProps {
-  hasError: boolean;
-}
-
-export const Container = styled.div<IInputProps>`
+export const Container = styled.div`
   h1 {
     text-align: center;
     font-size: 1.3rem;
@@ -20,54 +16,6 @@ export const Container = styled.div<IInputProps>`
     margin: 0 auto;
     margin-top: 5rem;
     padding: 2rem;
-
-    input {
-      width: 100%;
-      height: 2.6rem;
-      padding: 0.7rem;
-      background-color: ${(props) =>
-        props.hasError ? "rgba(255, 99, 71, 0.2)" : "var(--white-200)"};
-      border-radius: 0.3rem;
-      font-size: 1rem;
-      font-weight: 500;
-      border: 0.3px solid var(--white-300);
-      transition: all ease 0.2s;
-      outline: none;
-
-      :focus {
-        outline: 0.3px solid var(--blue-200);
-        border: none;
-
-        &:hover {
-          border: none;
-        }
-      }
-
-      &:hover {
-        background-color: var(--white-300);
-        border: 0.3px solid var(--gray-100);
-
-        ::placeholder {
-          color: var(---gray-100);
-          transition: all ease 0.2s;
-        }
-
-        ::-ms-input-placeholder {
-          color: var(---gray-100);
-          transition: all ease 0.2s;
-        }
-      }
-
-      ::placeholder {
-        color: var(--gray-100);
-        opacity: 0.4;
-      }
-
-      ::-ms-input-placeholder {
-        color: var(--gray-100);
-        opacity: 0.4;
-      }
-    }
 
     input[type="submit"] {
       margin-top: 1rem;
@@ -117,5 +65,59 @@ export const Container = styled.div<IInputProps>`
         border: 0.3px solid var(--gray-100);
       }
     }
+  }
+`;
+
+interface IInputProps {
+  hasError: boolean;
+}
+
+export const CustomIput = styled.input<IInputProps>`
+  width: 100%;
+  height: 2.6rem;
+  padding: 0.7rem;
+  background-color: ${(props) =>
+    props.hasError ? "rgba(255, 99, 71, 0.2)" : "var(--white-200)"};
+  border-radius: 0.3rem;
+  font-size: 1rem;
+  font-weight: 500;
+  border: 0.3px solid var(--white-300);
+  transition: all ease 0.2s;
+  outline: none;
+
+  :focus {
+    outline: 0.3px solid var(--blue-200);
+    outline: ${(props) =>
+      props.hasError ? "0.3px solid red" : "0.3px solid var(--blue-200)"};
+    border: none;
+
+    &:hover {
+      border: none;
+    }
+  }
+
+  &:hover {
+    background-color: var(--white-300);
+    border: 0.3px solid var(--gray-100);
+
+    ::placeholder {
+      color: var(---gray-100);
+      transition: all ease 0.2s;
+    }
+
+    ::-ms-input-placeholder {
+      color: var(---gray-100);
+      transition: all ease 0.2s;
+    }
+  }
+
+  ::placeholder {
+    color: var(--gray-100);
+    opacity: 0.4;
+  }
+
+  ::-ms-input-placeholder {
+    color: var(--gray-100);
+    opacity: 0.4;
   }
 `;
