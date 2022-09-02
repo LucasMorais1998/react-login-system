c32852fc903bce28d23972eb58180a1be3768463
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-09-01 22:12:01 -0300

    add pages/Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+import { Header } from "../../components/Header";
+
+import { Container } from "./styles";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Container>
+        <h1>Login System</h1>
+        <p>
+          <span>
+            <Link to="/login">Login</Link>&nbsp;
+          </span>
+          to access our content.
+        </p>
+        <p>
+          Don't have a account? Create new account&nbsp;
+          <span>
+            <Link to="/register">here</Link>
+          </span>
+          .
+        </p>
+      </Container>
+    </>
+  );
+};
+
+export { Home };
