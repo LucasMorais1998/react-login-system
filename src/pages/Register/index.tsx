9de34052c7e81f84c0fdf3df76c4c5f2fc7ad40d
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-09-01 21:46:20 -0300

    feat pages/Register

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,5 +1,88 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+
+import type { InputsTypes } from "../../types/Input";
+
+import { Header } from "../../components/Header";
+
+import { CustomIput } from "../../styles/customInput";
+import { Container } from "./styles";
+
+interface RegisterInputs extends InputsTypes {
+  firstName: string;
+  lastName: string;
+  company?: string;
+}
+
 const Register = () => {
-  return <div></div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterInputs>();
+
+  const onSubmit: SubmitHandler<RegisterInputs> = () => {
+    console.log("data");
+  };
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h1>Create an account</h1>
+          <label htmlFor="firstName">
+            <CustomIput
+              hasError={Boolean(errors.firstName)}
+              id="firstName"
+              type="text"
+              placeholder="First name"
+              {...register("firstName", { required: true })}
+            />
+          </label>
+
+          <label htmlFor="lastName">
+            <CustomIput
+              hasError={Boolean(errors.lastName)}
+              id="lastName"
+              type="text"
+              placeholder="Last name"
+              {...register("lastName", { required: true })}
+            />
+          </label>
+
+          <label htmlFor="company">
+            <CustomIput
+              id="company"
+              type="text"
+              placeholder="Company"
+              {...register("company", { required: false })}
+            />
+          </label>
+
+          <label htmlFor="email">
+            <CustomIput
+              hasError={Boolean(errors.email)}
+              id="email"
+              type="email"
+              placeholder="Email address"
+              {...register("email", { required: true })}
+            />
+          </label>
+
+          <label htmlFor="password">
+            <CustomIput
+              hasError={Boolean(errors.password)}
+              id="password"
+              type="password"
+              placeholder="Password"
+              {...register("password", { required: true })}
+            />
+          </label>
+          <input type="submit" value="Sign up" />
+        </form>
+      </Container>
+    </>
+  );
 };
 
 export { Register };
