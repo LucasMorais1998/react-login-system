598ef03cf93a6c8df8f1e0960f8a6a917c87f71f
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-09-05 23:24:16 -0300

    feat modal

--- src/pages/Private/index.tsx
+++ src/pages/Private/index.tsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import { BsCameraFill } from "react-icons/bs";
 
 import { Header } from "../../components/Header";
+import { UploadProfileImageModal } from "../../components/UploadProfileImageModal";
 
 import { Container } from "./styles";
 
 const Private = () => {
+  const [isUploadProfileImageModalOpen, setIsUploadProfileImageModalOpen] =
+    useState(false);
+
+  const handleOpenUploadProfileImageModal = () => {
+    setIsUploadProfileImageModalOpen(true);
+  };
+
+  const handleCloseUploadProfileImageModal = () => {
+    setIsUploadProfileImageModalOpen(false);
+  };
+
   return (
     <>
       <Header />
@@ -12,7 +25,10 @@ const Private = () => {
         <h1>Private Page</h1>
         <div className="profile-image">
           <img src="https://placehold.jp/150x150.png" alt="profile image" />
-          <div className="change-profile-image">
+          <div
+            className="change-profile-image"
+            onClick={handleOpenUploadProfileImageModal}
+          >
             <BsCameraFill className="change-profile-image-icon" />
           </div>
         </div>
@@ -26,6 +42,10 @@ const Private = () => {
           Email: <span>email</span>
         </p>
       </Container>
+      <UploadProfileImageModal
+        isOpen={isUploadProfileImageModalOpen}
+        onRequestClose={handleCloseUploadProfileImageModal}
+      />
     </>
   );
 };
