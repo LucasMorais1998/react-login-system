82bad14c48916abe9de5cfa51b9bcf7b1bc007ab
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-09-05 23:23:48 -0300

    config modal

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom";
 
+import Modal from "react-modal";
+
 import { Header } from "../../components/Header";
 
 import { Container } from "./styles";
+
+Modal.setAppElement("#root");
 
 const Home = () => {
   return (
