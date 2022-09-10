bda8627c50411b2e9dc72a5f978a74b386f0ec7b
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-09-09 22:43:10 -0300

    feat contexts/Auth

---
+++ src/contexts/Auth/AuthContext.tsx
@@ -0,0 +1,12 @@
+import { createContext } from "react";
+
+import { InputsTypes } from "../../types/Input";
+import { User } from "../../types/User";
+
+export type AuthContextType = {
+  user: User | null;
+  signin: ({ email, password }: InputsTypes) => Promise<boolean>;
+  signout: () => void;
+};
+
+export const AuthContext = createContext<AuthContextType>(null!);
