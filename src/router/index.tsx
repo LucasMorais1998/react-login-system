50eca7b8b7bf38e22ec301da119182c12dcd8af1
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-09-09 22:41:02 -0300

    fear router/RequireAuth

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,20 +1,32 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home } from "../pages/Home";
 
+import { AuthProvider } from "../providers/Auth/AuthProvider";
+
+import { RequireAuth } from "../contexts/Auth/RequireAuth";
+import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
 import { Private } from "../pages/Private";
 import { Register } from "../pages/Register";
 
 const Router = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/private" element={<Private />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/private"
+            element={
+              <RequireAuth>
+                <Private />
+              </RequireAuth>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 };
 
