cd0c4ec482513013713db47e59a5824305ace4d2
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-09-10 16:57:08 -0300

    feat components/UploadProfileImageModal styles

--- src/components/UploadProfileImageModal/styles.ts
+++ src/components/UploadProfileImageModal/styles.ts
@@ -1,15 +1,36 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+interface IContainerProps {
+  isUploading: boolean;
+}
+
+export const Container = styled.div<IContainerProps>`
   img {
+    width: 150px;
+    height: 150px;
+    object-fit: cover;
+    margin-top: 2rem;
     border-radius: 50%;
+    animation: ${(props) =>
+      props.isUploading ? "rotation 4s infinite linear;" : ""};
+  }
+
+  @keyframes rotation {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(359deg);
+    }
   }
 `;
 
-export const ModalOptions = styled.div`
+export const ModalOptions = styled.form`
   display: flex;
-  justify-content: space-around;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
   align-items: center;
+  gap: 1rem;
   margin-top: 2rem;
 
   .delete-image-icon {
@@ -23,6 +44,23 @@ export const ModalOptions = styled.div`
 
     &:hover {
       background-color: var(--gray-300);
+    }
+  }
+
+  .submit-image-icon {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    width: 90px;
+    height: 35px;
+    border: none;
+    background-color: var(--blue-100);
+    color: var(--white-100);
+    border-radius: 0.3rem;
+
+    &:hover {
+      background-color: var(--blue-200);
     }
   }
 `;
