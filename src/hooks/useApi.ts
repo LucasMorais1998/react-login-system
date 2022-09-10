c511dac6a1b60b06ff99c138f9d5482913a9ab8e
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-09-09 22:42:45 -0300

    feat hooks/useApi

---
+++ src/hooks/useApi.ts
@@ -0,0 +1,75 @@
+import { api } from "../services/Api/api";
+import { User } from "../types/User";
+
+import { InputsTypes } from "./../types/Input";
+
+type validateTokenTypes = {
+  token: string;
+  user: User;
+};
+
+export const useApi = () => ({
+  validateToken: async (token: string): Promise<validateTokenTypes> => {
+    // mock
+    return {
+      token,
+      user: {
+        id: 1,
+        email: "john.doe@email.com",
+        firstName: "John",
+        lastName: "Doe",
+        company: "Lorem Ipsum",
+      },
+    };
+
+    const response = await api.post("/validate", {
+      token,
+    });
+
+    return response.data;
+  },
+
+  signin: async ({
+    email,
+    password,
+  }: InputsTypes): Promise<validateTokenTypes> => {
+    // mock
+    return {
+      user: {
+        id: 1,
+        email: "john.doe@email.com",
+        firstName: "John",
+        lastName: "Doe",
+        company: "Lorem Ipsum",
+      },
+      token: "12345678",
+    };
+
+    const response = await api.post("/signin", {
+      email,
+      password,
+    });
+
+    const user: User = await response.data.user;
+
+    return {
+      user: {
+        id: 1,
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        company: user.company,
+      },
+      token: "token",
+    };
+  },
+
+  logout: async (): Promise<Boolean> => {
+    // mock
+    return false;
+
+    const response = await api.post("/logout");
+
+    return response.data;
+  },
+});
