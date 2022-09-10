9577a1257b7eda36f5483b3d5ac352dd3ca0fd60
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-09-10 16:54:38 -0300

    feat router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -7,25 +7,28 @@ import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
 import { Private } from "../pages/Private";
 import { Register } from "../pages/Register";
+import { ProfileImageProvider } from "../providers/ProfileImage/ProfileImageProvider";
 
 const Router = () => {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/private"
-            element={
-              <RequireAuth>
-                <Private />
-              </RequireAuth>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
+      <ProfileImageProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/private"
+              element={
+                <RequireAuth>
+                  <Private />
+                </RequireAuth>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </ProfileImageProvider>
     </AuthProvider>
   );
 };
