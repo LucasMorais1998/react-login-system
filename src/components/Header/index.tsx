9269ee999f5d8bb77b987f16a75512ade7ab89f9
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-09-09 22:44:04 -0300

    feat components/Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,51 @@
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+
+import { Link, useNavigate } from "react-router-dom";
+
+import { AuthContext } from "../../contexts/Auth/AuthContext";
 
 import { Container } from "./styles";
 
 const Header = () => {
+  const { user, signout } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await signout();
+
+    navigate("/");
+  };
+
   return (
     <Container>
       <nav>
         <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-          <li>
-            <Link to="/private">Private</Link>
-          </li>
+          {!user ? (
+            <>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/private">Private</Link>
+              </li>
+              <li>
+                {user && (
+                  <Link to="/" onClick={handleLogout}>
+                    Sair
+                  </Link>
+                )}
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </Container>
